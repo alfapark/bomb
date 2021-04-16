9df5cf4c2cb312f613cce36948a495de873c2d1b
Jan Pokorný <pokorj54@fit.cvut.cz>
2021-04-16 16:49:01 +0200

    pins to separate file

--- bomb.c/bomb.c.ino
+++ bomb.c/bomb.c.ino
@@ -1,3 +1,5 @@
+#include "pins.hpp"
+
 //snipping cables
 
 
@@ -53,39 +55,6 @@ void setup() {
   //trololo
   pinMode(TButton, INPUT_PULLUP);
   pinMode(Tmeter, OUTPUT);
-}
-
-void read_pins(int n, int * pins, int * outputs){
-  for(int i = 0; i < n; ++i){
-    outputs[i] = digitalRead(pins[i]);
-  }
-}
-
-void copy_pins(int n, int * from, int * to){
-  for(int i = 0; i <n; ++i){
-    to[i] = from[i];
-  }
-}
-
-bool are_same(int n, int * a, int * b){
-  for(int i = 0; i < n; ++i){
-    if(a[i] != b[i]){
-      return false;
-    }
-  }
-  return true;
-}
-
-void print_pins(int n, int * pins){
-  for(int i = 0; i < n; ++i){
-    Serial.print(pins[i]);
-  }
-}
-
-void set_kth_pin_low_only(int n, int k, int * pins){
-  for(int i = 0; i < n; ++i){
-    digitalWrite(pins[i], i == k ? LOW : HIGH);
-  }
 }
 
 int last_inputs_s[5] = {0};
---
+++ bomb.c/pins.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+void read_pins(int n, int * pins, int * outputs){
+  for(int i = 0; i < n; ++i){
+    outputs[i] = digitalRead(pins[i]);
+  }
+}
+
+void copy_pins(int n, int * from, int * to){
+  for(int i = 0; i <n; ++i){
+    to[i] = from[i];
+  }
+}
+
+bool are_same(int n, int * a, int * b){
+  for(int i = 0; i < n; ++i){
+    if(a[i] != b[i]){
+      return false;
+    }
+  }
+  return true;
+}
+
+void print_pins(int n, int * pins){
+  for(int i = 0; i < n; ++i){
+    Serial.print(pins[i]);
+  }
+}
+
+void set_kth_pin_low_only(int n, int k, int * pins){
+  for(int i = 0; i < n; ++i){
+    digitalWrite(pins[i], i == k ? LOW : HIGH);
+  }
+}
