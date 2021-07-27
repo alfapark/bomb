7d58675613851df0aaab3875cb2a4912da30ff25
Jan Pokorný <pokorj54@fit.cvut.cz>
2021-07-27 13:58:58 +0200

    fixed initial connection module value

--- bomb.c/connecting_module.hpp
+++ bomb.c/connecting_module.hpp
@@ -11,31 +11,31 @@ ConnectingModule( int success_led_pin, int fail_led_pin, int input_pins[N_CONNEC
     int output_pins[N_CONNECT_PINS], int win_mask[N_CONNECT_PINS][N_CONNECT_PINS]): 
   Module(success_led_pin, fail_led_pin), current_cycle(0), last_measurement(0){
     for(int i = 0; i < N_CONNECT_PINS; ++i){
-      pinMode(input_pins[i], INPUT_PULLUP); 
-      pinMode(output_pins[i], OUTPUT); 
-      set_all_pins_to_value(N_CONNECT_PINS, last[i], 0);
+      set_array_to_values(N_CONNECT_PINS, last[i], 1);
       copy_pins(N_CONNECT_PINS, win_mask[i], mask[i]);
       debounced[i] = 0;
     }
+    set_mode(N_CONNECT_PINS, input_pins, INPUT_PULLUP);
+    set_mode(N_CONNECT_PINS, output_pins, INPUT_PULLUP);
     copy_pins(N_CONNECT_PINS, input_pins, pins_input);
     copy_pins(N_CONNECT_PINS, output_pins, pins_output);
   }
   virtual void run();
   
 private: 
-  bool check_win();
-  bool check_fail();
-
-  int current_cycle;
-  unsigned long last_measurement;
   unsigned long debounced[N_CONNECT_PINS]; //debounce for each cycle separately
+  unsigned long last_measurement;
   int pins_input[N_CONNECT_PINS];
   int pins_output[N_CONNECT_PINS];
   int last[N_CONNECT_PINS][N_CONNECT_PINS];
   int mask[N_CONNECT_PINS][N_CONNECT_PINS];
+  int current_cycle;
 
   static const unsigned long cycle_check_length = 10;
   static const unsigned long CONN_PENALTY = 120000;
+  
+  bool check_win();
+  bool check_fail();
 };
 
 
