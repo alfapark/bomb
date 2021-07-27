30e9d4bddc56408ab032145a5176fdc7759c572b
Jan Pokorný <pokorj54@fit.cvut.cz>
2021-07-27 10:55:36 +0200

    corrected blank state if turned off switches

--- bomb.c/switch_module.hpp
+++ bomb.c/switch_module.hpp
@@ -31,8 +31,10 @@ void SwitchModule::run(){
   int to_display[N_SWITCH_PINS];
   set_array_to_values(N_SWITCH_PINS, to_display, 0);
 
+  int switches_inputs[N_SWITCH_PINS];
+  read_pins(N_SWITCH_PINS, pins_switches, switches_inputs);
   for(int i = 0; i < N_SWITCH_PINS; ++i){
-    if(digitalRead(pins_switches[i]) == 0){
+    if(switches_inputs[i] == 0){
       for(int j = 0; j < N_SWITCH_PINS; ++j){
         to_display[j] = to_display[j] ^ switch_leds[i][j];
       }
@@ -44,9 +46,10 @@ void SwitchModule::run(){
     solved_flag = solved_flag && to_display[i];
   }
   int all_turned_off[N_SWITCH_PINS] = {1,1,1,1,1,1};
+  
   if(solved_flag){
     success();
-  }else if(are_same(N_SWITCH_PINS, pins_switches, all_turned_off)){
+  }else if(are_same(N_SWITCH_PINS, switches_inputs, all_turned_off)){
     blank_state();
   } else{
     fail();
